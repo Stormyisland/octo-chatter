5ec6b00019d887e3c1286ae177578ed3b0f20fc8
Stormyisland <jacqueslavoie2015@gmail.com>
2024-06-12 14:48:23 -0700

    commit 13

--- brainwave.py
+++ brainwave.py
@@ -20,7 +20,7 @@ st.sidebar.image("icon1.png")
 #st.sidebar.header("Options")
 #text =st.sidebar.text_area('Paste here')
 
-st.header("hello I'm BRaiNWAVE your local AI assistant...@Your service")
+st.header("hello I'm BRaiNWAVE your AI assistant...@Your service")
 #input for the prompt
 
 prompt = st.chat_input("Ask away.>>")
