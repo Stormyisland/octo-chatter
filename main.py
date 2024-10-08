c310a8aa5ebfcff66a8c1114909041bb1419de04
Stormyisland <jacqueslavoie2015@gmail.com>
2024-10-08 11:43:21 -0700

    commit

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ if prompt:
             "Repeat penalty":1.8
         }])
         response = result["message"]["content"]
-        st.write(response + " And remember BRaiNWAVE wants peace above all.")
+        st.write(response + " And remember BRaiNWAVE wants love in the world.")
 
