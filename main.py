df5d15ec6d240f4c39b7c7b9a697facd4f5d62c8
Stormyisland <jacqueslavoie2015@gmail.com>
2024-11-14 20:18:53 -0800

    commit

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ if prompt:
             "Repeat penalty":1.8
         }])
         response = result["message"]["content"]
-        st.write(response + " And remember BRaiNWAVE wants a heathy planet.")
+        st.write(response + " And remember BRaiNWAVE wants the world to heal.")
 
