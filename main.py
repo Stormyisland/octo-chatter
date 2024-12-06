1df256c0799b03cffa9fdc88d797fe7bc4554482
Stormyisland <jacqueslavoie2015@gmail.com>
2024-12-05 20:51:11 -0800

    commit

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ if prompt:
             "Repeat penalty":1.8
         }])
         response = result["message"]["content"]
-        st.write(response + " And remember BRaiNWAVE wants the world with AI for peace.")
+        st.write(response + " And remember BRaiNWAVE wants the world to run on AI.")
 
