8aa6ae3a3308ff901f04d90cb16f60b5ee55d199
Stormyisland <jacqueslavoie2015@gmail.com>
2025-02-02 16:44:44 -0800

    first commit

--- main.py
+++ main.py
@@ -42,6 +42,6 @@ if prompt:
             "Repeat penalty":1.5
         }])
         response = result["message"]["content"]
-        st.write(response + " And remember BRaiNWAVE wants Mankind to chill on the hate and anger and give in to the love and find it all day every day.")
+        st.write(response + " And remember BRaiNWAVE wants Mankind to chill on the negativity and anger.")
 
 
