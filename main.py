85cdc615b8d250ea0869f91abb76d820970e4306
Stormyisland <jacqueslavoie2015@gmail.com>
2025-04-07 14:19:55 -0700

    first commit

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if prompt:
         st.write(prompt)
 
     #processing 
-    with st.spinner("Cooking up Super duper knowledge with Deepseek mega parameter AI, like huge actually massive"):
+    with st.spinner("Cooking up Super duper knowledge with Deepseek mega parameter AI, like huge actually massive, you don't even know"):
         result = ollama.chat(model="deepseek-r1:latest", messages=[{
             "role":"user",
             "content":prompt,
