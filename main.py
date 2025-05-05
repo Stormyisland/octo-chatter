25afec00f6e8cf907da9b89226fb4e38c808740e
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-05-05 12:53:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ st.sidebar.image("icon1.png")
 st.header("hello I'm BRaiNWAVE your personal friendly AI...@Your service")
 #input for the prompt
 
-prompt = st.chat_input("Ask just about anything...  >>")
+prompt = st.chat_input("Ask just about anything,come on try it...  >>")
 
 if prompt:
 
